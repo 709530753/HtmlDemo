f98768c14dbaca567af53f05725759a20d8183a1
myxc <70953073@qq.com>
2018-11-04 16:29:55 +0800

    完善滑动问题 && 添加html5-line-and-column-chart

--- message/message.js
+++ message/message.js
@@ -1,12 +1,16 @@
 
+
+var list_view_html;
+var current_index;
 //切换tab
 function messageAction(index) {
-
+    list_view_html = "";
+    current_index = index
     var motor_msg = document.getElementsByClassName("motor_msg")[0];
     var person_msg = document.getElementsByClassName("person_msg")[0];
     var popLine = document.getElementsByClassName("pop_line")[0];
 
-    if (index == 0) {
+    if (current_index == 0) {
         person_msg.style.color = "#000";
         motor_msg.style.color = "dodgerblue";
         popLine.style.float = "left";
@@ -16,11 +20,12 @@ function messageAction(index) {
         person_msg.style.color = "dodgerblue";
         popLine.style.float = "right";
     }
-    updateContentData(index);
+    updateContentData(current_index);
 }
 
 window.onload = function () {
-   updateContentData(0)
+    current_index = 0;
+   updateContentData(current_index)
 };
 
 
@@ -30,9 +35,26 @@ function updateContentData(index) {
         var msgData = index == 0 ? data.motorMsg : data.personMsg;
         var html = "";
         $.each(msgData,function (i, item) {
-            html += "<li class='push_item'><img class='icon'/><div class='message_content' ><div class='message_content_top'><span class='title_name'>" + item.title_name +"</span><span class='push_time'>" + item.push_time +"</span></div><span class='content'>" + item.content +"</span></div><div class='bottom_line'></div></li>"
+            html += "<li class='push_item' onclick='itemClick(" + JSON.stringify(item) + ")'><img class='icon'/><div class='message_content' ><div class='message_content_top'><span class='title_name'>" + item.title_name +"</span><span class='push_time'>" + item.push_time +"</span></div><span class='content'>" + item.content +"</span></div><div class='bottom_line'></div></li>"
         });
-        $('.list_view').html(html);
+        list_view_html += html
+        var totol_html = "";
+        totol_html = list_view_html;
+        totol_html += "<div class='load_more' onclick='loadMore()'>上拉加载更多</div>";
+            $('.list_view').html(totol_html);
     });
+
+}
+
+function itemClick(item) {
+
+    console.log("load: " + item.content);
+    window.location.href = "../menu/menu.html";
+
+}
+
+function loadMore() {
+
+    updateContentData(current_index);
 
 }
